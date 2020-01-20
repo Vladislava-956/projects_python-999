6ba701059777407ef8b5f59d410770bfd76d107d
Vladislava <59018546+Vladislava-956@users.noreply.github.com>
2020-01-20 14:33:48 +0300

    Add files via upload

--- Projekt_1_3_47.py
+++ Projekt_1_3_47.py
@@ -1,70 +1,77 @@
 import random
+
+
 def good_function():
     digit = random.randint(10, 10000)
     digit_list = list(str(digit))
-    digit_list_Z = list(digit_list[::-1])
+    digit_list_z = list(digit_list[::-1])
     len_digit = len(digit_list)
-    def max_digit():
-        assert digit_list
-        m = digit_list[0]
-        for i in digit_list:
-            if i > m:
-                m = i
-        return int(m) + 1
-    random_Number_System = random.randint(max_digit(), 10)
+    digit_max = max([int(i) for i in str(digit)])
+    random_number_system = random.randint(digit_max+1, 10)
+    _Number_System_in_degree = []
+    for z in range(len_digit):
+        z = str(random_number_system ** int(z))
+        _Number_System_in_degree.append(z)
 
-    p = []
-    for i in range(len_digit):
-        i = str(random_Number_System ** int(i))
-        p.append(i)
-    j = []
-    for i in range(len_digit):
-        i = str(int(p[i]) * int(digit_list_Z[i]))
-        j.append(i)
-    def Sum():
-        Sum = 0
-        for i in j:
-            i = int(i)
-            Sum = Sum + i
-        return Sum
-    digit1 = Sum()
-    def dv_digit(digit1, to_base=2, from_base=10):
-        if isinstance(digit1, str):
-            n = int(digit1, from_base)
+    _Multiplication = []
+    for y in range(len_digit):
+        y = str(int(_Number_System_in_degree[y]) * int(digit_list_z[y]))
+        _Multiplication.append(y)
+
+    def plus_multiplication():
+        plus = 0
+        for x in _Multiplication:
+            x = int(x)
+            plus = plus + x
+        return plus
+
+    digit_10_number_system = plus_multiplication()
+
+    def digit_2_number_system(digit_10_number_system, to_base=2, from_base=10):
+        if isinstance(digit_10_number_system, str):
+            n = int(digit_10_number_system, from_base)
         else:
-            n = int(digit1)
+            n = int(digit_10_number_system)
         alp = "0123456789"
         if n < to_base:
             return alp[n]
         else:
-            return dv_digit(n // to_base, to_base) + alp[n % to_base]
-    massiv_dv_digit = list(str(dv_digit(digit1, to_base=2, from_base=10)))
-    Col_vo_Ed = int(massiv_dv_digit.count('1'))
-    if(Col_vo_Ed >= M) and (Col_vo_Ed <= N):
-        return str(digit) + '_' + str(random_Number_System)
+            return digit_2_number_system(n // to_base, to_base) + alp[n % to_base]
+
+    lis_digit_2_number_system = list(str(digit_2_number_system(digit_10_number_system, to_base=2, from_base=10)))
+    count_1_of_array = int(lis_digit_2_number_system.count('1'))
+    if(count_1_of_array >= M) and (count_1_of_array <= N):
+        return str(digit) + '_' + str(random_number_system)
     else:
-        n = []
-        n.append(str(digit) + '_' + str(random_Number_System))
-        return n
+        not_suitable_digits = []
+        not_suitable_digits.append(str(digit) + '_' + str(random_number_system))
+        return not_suitable_digits
+
+
 count = int(input('Количество чисел: '))
 M = random.randint(1, 10)
 N = random.randint(M + 1, 20)
-h = []
+digits_all = []
 for i in range(1, count + 1):
-    h.append(str(good_function()))
-k = []
-for i in h:
+    digits_all.append(str(good_function()))
+_not_suitable_digits = []
+for i in digits_all:
     if (''.join([str(elem) for elem in i]).count('[')) != 1:
-        k.append(''.join([str(elem) for elem in i]))
-h = str(h)
-h = h.strip('[')
-h = h.strip(']')
-k = str(k)
-k = k.strip('[')
-k = k.strip(']')
-Col_vo_True_Digit = int(k.count(',')) + 1
+        _not_suitable_digits.append(''.join([str(elem) for elem in i]))
+digits_all = str(digits_all)
+digits_all = digits_all.strip('[')
+digits_all = digits_all.strip(']')
+_not_suitable_digits = str(_not_suitable_digits)
+_not_suitable_digits = _not_suitable_digits.strip('[')
+_not_suitable_digits = _not_suitable_digits.strip(']')
+Col_vo_True_Digit = int(_not_suitable_digits.count(',')) + 1
 if Col_vo_True_Digit == 1:
     Col_vo_True_Digit = 0
 else:
     Col_vo_True_Digit = Col_vo_True_Digit
-print('Количество единиц от' + '%+2s ' % str(M) + 'до' + '%+3s ' % str(N) + '\n' '-----------------' '\n' 'Задание.' '\n' 'Из набора' + '%+2s ' % str(count) + 'целых чисел'+ '%+4s ' % str(h) + '\n'  'записанных в разных системах счисления выбрать числа, запись которых' '\n' 'в двоичной системе счисления имеет от' + '%+2s ' % str(M) + 'до' + '%+3s ' % str(N) + 'единиц.' '\n' '-----------------' '\n' 'Ответ:' '\n' 'Количество подходящих чисел' + '%+4s ' % str(Col_vo_True_Digit) + '\n' + '%+3s ' % str(k))
+print('-----------------' '\n' 'Задание.')
+print('Из набора ' + '%+2s ' % str(count) + 'целых чисел ' + '%+4s ' % str(digits_all))
+print('записанных в разных системах счисления выбрать числа, запись которых')
+print('в двоичной системе счисления имеет от' + '%+2s ' % str(M) + 'до' + '%+3s ' % str(N) + 'единиц.')
+print('-----------------' '\n' 'Ответ:' '\n' 'Количество подходящих чисел' + '%+4s ' % str(Col_vo_True_Digit))
+print('%+3s ' % str(_not_suitable_digits))
