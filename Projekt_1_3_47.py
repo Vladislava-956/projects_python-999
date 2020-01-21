627ce2f2157671d0beaa37ce0408bfa9b3a15ee1
Vladislava <59018546+Vladislava-956@users.noreply.github.com>
2020-01-21 15:24:33 +0300

    Add files via upload

--- Projekt_1_3_47.py
+++ Projekt_1_3_47.py
@@ -40,38 +40,36 @@ def good_function():
 
     lis_digit_2_number_system = list(str(digit_2_number_system(digit_10_number_system, to_base=2, from_base=10)))
     count_1_of_array = int(lis_digit_2_number_system.count('1'))
-    if(count_1_of_array >= M) and (count_1_of_array <= N):
+    if(count_1_of_array > M) and (count_1_of_array < N):
         return str(digit) + '_' + str(random_number_system)
     else:
-        not_suitable_digits = []
-        not_suitable_digits.append(str(digit) + '_' + str(random_number_system))
-        return not_suitable_digits
+        return str(digit) + '_' + str(random_number_system) + str(' ')
 
 
 count = int(input('Количество чисел: '))
-M = random.randint(1, 10)
-N = random.randint(M + 1, 20)
+M = random.randint(1, 5)
+N = random.randint(M + 1, 10)
+
 digits_all = []
 for i in range(1, count + 1):
     digits_all.append(str(good_function()))
-_not_suitable_digits = []
+
+_suitable_digits = []
 for i in digits_all:
-    if (''.join([str(elem) for elem in i]).count('[')) != 1:
-        _not_suitable_digits.append(''.join([str(elem) for elem in i]))
+    if (''.join([str(elem) for elem in i]).count(' ')) != 1:
+        _suitable_digits.append(''.join([str(elem) for elem in i]))
+
 digits_all = str(digits_all)
 digits_all = digits_all.strip('[')
 digits_all = digits_all.strip(']')
-_not_suitable_digits = str(_not_suitable_digits)
-_not_suitable_digits = _not_suitable_digits.strip('[')
-_not_suitable_digits = _not_suitable_digits.strip(']')
-Col_vo_True_Digit = int(_not_suitable_digits.count(',')) + 1
-if Col_vo_True_Digit == 1:
-    Col_vo_True_Digit = 0
-else:
-    Col_vo_True_Digit = Col_vo_True_Digit
+
+suitable_digits = str(_suitable_digits)
+Col_vo_True_Digit = int(suitable_digits.count('_'))
+
 print('-----------------' '\n' 'Задание.')
 print('Из набора ' + '%+2s ' % str(count) + 'целых чисел ' + '%+4s ' % str(digits_all))
 print('записанных в разных системах счисления выбрать числа, запись которых')
 print('в двоичной системе счисления имеет от' + '%+2s ' % str(M) + 'до' + '%+3s ' % str(N) + 'единиц.')
 print('-----------------' '\n' 'Ответ:' '\n' 'Количество подходящих чисел' + '%+4s ' % str(Col_vo_True_Digit))
-print('%+3s ' % str(_not_suitable_digits))
+for i in _suitable_digits:
+    print(i)
